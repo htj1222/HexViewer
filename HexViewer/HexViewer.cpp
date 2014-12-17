4f10a42793032021b71d3c39bad4859bdb3acd01
htj1222 <htj1222@gmail.com>
2014-12-17 17:17:33 +0900

    1

--- HexViewer/HexViewer.cpp
+++ HexViewer/HexViewer.cpp
@@ -1,9 +1,7 @@
 // HexViewer.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
-//
-
-
 #include "stdafx.h"
 #include <stdio.h>
+#include <iostream>
 #include <stdlib.h> // system()
 #include <string.h> // memset()
 #include <ctype.h>  // tolower()
@@ -11,12 +9,14 @@
 
 //char* fileName = "UHD_fin2.mpg";
 char* fileName = "notake.mpg";
+using namespace std;
 
 int _tmain(int argc, const char* argv[])
 {
 	__int64 currentPacket = 0, packetCount = 0;
 	FILE *pFile;
 	function _function;
+	char key;
 
 	/*
 	if (argc != 2) {
@@ -37,20 +37,20 @@ int _tmain(int argc, const char* argv[])
 		system("cls");
 
 		printf("File: %s\n", fileName);
-		printf("\nPacket %I64u/%I64u < (이전) > (다음) Q (종료)?\n\n", currentPacket + 1, packetCount);
+		
 
 		_function.printHex(pFile, currentPacket);
 		_function.packetInfo.printInfo();
 
-		switch (tolower(getch())) {
-	  case ',':
+		printf("\nPacket %I64u/%I64u 1 (이전) 2 (다음) Q (종료)? : ", currentPacket + 1, packetCount);
+		cin >> key;
+
+		switch (tolower(key)) {
+	  case '1':
 		  if (currentPacket > 0) --currentPacket;
 		  break;
-	  case '.':
+	  case '2':
 		  if (currentPacket+1 < packetCount) ++currentPacket;
-		  break;
-	  case 'p':
-		  
 		  break;
 	  case 'q':
 		  fclose(pFile);
